876148aec68271623ba4049b8eec984a96163502
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-08-09 13:12:34 +0100

    EventGenre updated

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -9,7 +9,7 @@ const EventGenre = ({ events }) => {
             const getData = () => {
                 const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
                 const data = genres.map((genre) => {
-                    const value = events.filter((event) => event.summary.split('').includes(genre)).length;
+                    const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
                     return { name: genre, value };
                 })
                 return data;
