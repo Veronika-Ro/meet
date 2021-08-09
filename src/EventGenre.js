dffc78f3e07b93dc0a1c8d0cfb64671536405828
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-08-09 18:08:24 +0100

    EventGenre Update

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -5,17 +5,15 @@ const EventGenre = ({ events }) => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        setData(() => {
-            const getData = () => {
-                const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-                const data = genres.map((genre) => {
-                    const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
-                    return { name: genre, value };
-                })
-                return data;
-            };
-            getData();
-        });
+        const getData = () => {
+            const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+            const data = genres.map((genre) => {
+                const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
+                return { name: genre, value };
+            })
+            return data;
+        };
+        setData(() => getData());
     }, [events]);
 
     const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#aa33ff'];
